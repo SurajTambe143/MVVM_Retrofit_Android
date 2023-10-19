02173fd571254d4ac9ac6ff64a4112378ab50079
SurajTambe143 <tambesuraj413@gmail.com>
2023-10-19 18:56:50 +0530

    init commit

--- app/src/main/java/com/example/mvvmretrofit/repository/QuotesRepository.kt
+++ app/src/main/java/com/example/mvvmretrofit/repository/QuotesRepository.kt
@@ -1,22 +1,44 @@
 package com.example.mvvmretrofit.repository
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.mvvmretrofit.api.QuoteService
 import com.example.mvvmretrofit.model.QuotesResponse
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.lang.Exception
 
 class QuotesRepository(private val quoteService: QuoteService) {
 
-    private val quoteLiveData= MutableLiveData<QuotesResponse>()
+//    private val quoteLiveData= MutableLiveData<QuotesResponse>()
 
-    val quotes:LiveData<QuotesResponse>
-        get() = quoteLiveData
+//    val quotes:LiveData<QuotesResponse>
+//        get() = quoteLiveData
 
 
-    suspend fun getQuotes(page:Int) {
-        val result=quoteService.getQuotes(page)
-        if (result.body() !=null){
-            quoteLiveData.postValue(result.body())
-        }
-    }
+//    suspend fun getQuotes(page:Int) {
+//        val result=quoteService.getQuotes(page)
+//        if (result.body() !=null){
+//            quoteLiveData.postValue(result.body())
+//        }
+//    }
+
+    fun getQuotes(page:Int) =quoteService.getQuotes(page)
+
+//    fun getQuotes(page:Int) {
+//        try {
+//            compositeDisposable.add(
+//                quoteService.getQuotes(page)
+//                    .subscribeOn(Schedulers.io())
+//                    .subscribe({
+//                               quoteLiveData.postValue(it)
+//                    },{
+//                        Log.w("Error", it.message.toString() )
+//                    })
+//            )
+//        }catch (e:Exception){
+//            Log.w("Error Exception", e.message.toString())
+//        }
+//    }
 }
--- app/src/main/java/com/example/mvvmretrofit/viewmodel/MyViewModel.kt
+++ app/src/main/java/com/example/mvvmretrofit/viewmodel/MyViewModel.kt
@@ -1,20 +1,67 @@
 package com.example.mvvmretrofit.viewmodel
 
+import android.graphics.Movie
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mvvmretrofit.model.QuotesResponse
 import com.example.mvvmretrofit.repository.QuotesRepository
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MyViewModel(val repository: QuotesRepository): ViewModel() {
 
-    init {
-        viewModelScope.launch(Dispatchers.IO) {
-            repository.getQuotes(1)
+    private val quoteLiveData= MutableLiveData<QuotesResponse>()
+
+    val quotesLiveData:LiveData<QuotesResponse>
+        get() = quoteLiveData
+
+    lateinit var disposable: Disposable
+//    init {
+////        viewModelScope.launch(Dispatchers.IO) {
+////            repository.getQuotes(1)
+////        }
+//
+//        repository.getQuotes(1)
+//    }
+//    val quotes:LiveData<QuotesResponse>
+//        get() = repository.quotes
+
+
+    fun getQuotes(){
+        val response = repository.getQuotes(1)
+        response.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(getQuotesObserver())
+    }
+
+    private fun getQuotesObserver(): io.reactivex.rxjava3.core.Observer<QuotesResponse> {
+        return object : io.reactivex.rxjava3.core.Observer<QuotesResponse> {
+            override fun onSubscribe(d: Disposable) {
+                disposable=d
+            }
+
+            override fun onError(e: Throwable) {
+            }
+
+            override fun onComplete() {
+            }
+
+            override fun onNext(t: QuotesResponse) {
+                quoteLiveData.postValue(t)
+            }
+
         }
     }
-    val quotes:LiveData<QuotesResponse>
-        get() = repository.quotes
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.dispose()
+    }
+
 }
